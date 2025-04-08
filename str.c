8f05054bb133956effff5b417444c750e7cd3efd
Roland Smith <rsmith@xs4all.nl>
2025-04-08 19:10:44 +0200

    Format with astyle.

--- str.c
+++ str.c
@@ -4,42 +4,55 @@
 // Copyright © 2025 R.F. Smith <rsmith@xs4all.nl>
 // SPDX-License-Identifier: MIT
 // Created: 2025-04-07 22:53:56 +0200
-// Last modified: 2025-04-08T15:50:03+0200
+// Last modified: 2025-04-08T16:02:59+0200
 
 #include <assert.h>
 #include <string.h>
 
 #include "str.h"
 
-bool str_equals(Str a, Str b) {
-    return (a.len == b.len) && (!a.len || !memcmp(a.data, b.data, a.len));
+bool s8equals(s8 a, s8 b)
+{
+  return (a.len == b.len) && (!a.len || !memcmp(a.data, b.data, a.len));
 }
 
-Str str_lstrip(Str s) {
-    while (s.len && *s.data<=' ') {s.data++, s.len--;}
-    return s;
+s8 s8lstrip(s8 s)
+{
+  while (s.len && *s.data<=' ') {
+    s.data++, s.len--;
+  }
+  return s;
 }
 
-Str str_rstrip(Str s) {
-    while (s.len && s.data[s.len-1]<=' ') {s.len--;}
-    return s;
+s8 s8rstrip(s8 s)
+{
+  while (s.len && s.data[s.len-1]<=' ') {
+    s.len--;
+  }
+  return s;
 }
 
-Str str_strip(Str s) {
-    while (s.len && *s.data<=' ') {s.data++, s.len--;}
-    while (s.len && s.data[s.len-1]<=' ') {s.len--;}
-    return s;
+s8 s8strip(s8 s)
+{
+  while (s.len && *s.data<=' ') {
+    s.data++, s.len--;
+  }
+  while (s.len && s.data[s.len-1]<=' ') {
+    s.len--;
+  }
+  return s;
 }
 
-ptrdiff_t str_find(Str s, Str f) {
-    if (s.len == 0 || f.len == 0 || f.len > s.len) {
-        return -1;
-    }
-    ptrdiff_t maxoff = s.len - f.len;
-    for (ptrdiff_t off = 0; off <= maxoff ; off++) {
-      if (memcmp(s.data+off, f.data, f.len) == 0) {
-          return off;
-      }
-    }
+ptrdiff_t s8find(s8 s, s8 f)
+{
+  if (s.len == 0 || f.len == 0 || f.len > s.len) {
     return -1;
+  }
+  ptrdiff_t maxoff = s.len - f.len;
+  for (ptrdiff_t off = 0; off <= maxoff ; off++) {
+    if (memcmp(s.data+off, f.data, f.len) == 0) {
+      return off;
+    }
+  }
+  return -1;
 }
--- str.h
+++ str.h
@@ -4,27 +4,41 @@
 // Copyright © 2025 R.F. Smith <rsmith@xs4all.nl>
 // SPDX-License-Identifier: MIT
 // Created: 2025-04-07 22:53:56 +0200
-// Last modified: 2025-04-07T23:15:39+0200
+// Last modified: 2025-04-08T17:07:51+0200
 
 #pragma once
 #include <stddef.h>  // ptrdiff_t
 #include <stdbool.h>  // bool
 
-#define S(s)    (Str){s, sizeof(s)-1}
+#define S8(s)    (s8){s, sizeof(s)-1}
+
+// Note that these strings do not allocate memory.
+// Memory should be provided by e.g. an arena.
 
 typedef struct {
-    char *data;
-    ptrdiff_t len;
-} Str;
+  char *data;
+  ptrdiff_t len;
+} s8;
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-bool str_equals(Str a, Str b);
-Str str_lstrip(Str s);
-Str str_rstrip(Str s);
-Str str_strip(Str s);
+// Test two strings for equality of length and content.
+bool s8equals(s8 a, s8 b);
+
+// Remove whitespace from the left of a string.
+s8 s8lstrip(s8 s);
+
+// Remove whitespace from the right of a string.
+s8 s8rstrip(s8 s);
+
+// Remove whitespace from both ends of the string.
+s8 s8strip(s8 s);
+
+// Return the offset of the string f in the string s.
+// If not found, return -1.
+ptrdiff_t s8find(s8 s, s8 f);
 
 #ifdef __cplusplus
 }
